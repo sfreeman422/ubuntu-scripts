a4a5b972edf07732cfe1744a6cca2b0a9ce58a2b
Steve Freeman <sfreeman422@protonmail.com>
2025-07-30 13:41:37 -0400

    Upgrade zsh theme and font handling

--- zsh-theme.sh
+++ zsh-theme.sh
@@ -1,2 +1,5 @@
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git 
+cd nerd-fonts
+./install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
