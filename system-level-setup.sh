b22fde79c9d5ee601c9024a816213ac7fd711d22
Steve Freeman <sfreeman422@protonmail.com>
2025-07-30 14:22:31 -0400

    added change to hide desktop icons

--- system-level-setup.sh
+++ system-level-setup.sh
@@ -14,6 +14,9 @@ sudo dpkg-reconfigure -plow unattended-upgrades
 # Install fonts
 sudo apt install -y fonts-firacode fonts-powerline
 
+# Hide desktop icons
+gnome-extensions disable ding@rastersoft.com
+
 # Oh my ZSH
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
