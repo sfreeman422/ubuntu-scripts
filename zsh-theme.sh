4f487a09d148881d8f5cb16139228e347dce5122
Steve Freeman <sfreeman422@protonmail.com>
2025-07-30 13:42:39 -0400

    Updated zsh-theme to properly handle nerd-fonts

--- zsh-theme.sh
+++ zsh-theme.sh
@@ -1,5 +1,7 @@
 git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git 
 cd nerd-fonts
 ./install.sh
+cd ..
+rm -rf ./nerd-fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
