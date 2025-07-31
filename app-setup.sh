1ca4d11e8be55f4618176329c76a8b458b0bec78
Steve Freeman <sfreeman422@protonmail.com>
2025-07-30 21:40:39 -0400

    Converted to use slack debian package since the snap is buggy

--- app-setup.sh
+++ app-setup.sh
@@ -1,9 +1,14 @@
-# Install spotify slack discord chromium
-snap install spotify slack discord chromium
+# Install spotify discord chromium
+snap install spotify discord chromium
+
+curl -L -o ~/Downloads/slack-desktop-4.45.64-amd64.deb https://downloads.slack-edge.com/desktop-releases/linux/x64/4.45.64/slack-desktop-4.45.64-amd64.deb
+sudo apt install ~/Downloads/slack-desktop-4.45.64-amd64.deb -y
+sudo apt install --fix-broken -y
 
 # Install steam
 curl -L -o ~/Downloads/steam_latest.deb https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
 sudo apt install ~/Downloads/steam_latest.deb -y
+sudo apt install --fix-broken -y
 
 # Download ProtonMail Bridge
 curl -L -o ~/Downloads/protonmail-bridge_3.21.2-1_amd64.deb https://proton.me/download/bridge/protonmail-bridge_3.21.2-1_amd64.deb
