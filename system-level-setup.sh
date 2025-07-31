57a974b264a1abe3472ac4121d91383aa33b118f
Steve Freeman <sfreeman422@protonmail.com>
2025-07-31 00:01:13 -0400

    Added minimize on click to system-level-setup

--- system-level-setup.sh
+++ system-level-setup.sh
@@ -17,6 +17,9 @@ sudo apt install -y fonts-firacode fonts-powerline
 # Hide desktop icons
 gnome-extensions disable ding@rastersoft.com
 
+# Enable minimize on click for the dock
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
 # Oh my ZSH
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
